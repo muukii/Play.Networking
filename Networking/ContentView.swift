07614471037d3a74028569274b47eb17cd042926
Muukii <muukii.app@gmail.com>
2020-02-13 10:35:04 +0900

    Initial Commit

---
+++ Networking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Networking
+//
+//  Created by muukii on 2020/02/13.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
